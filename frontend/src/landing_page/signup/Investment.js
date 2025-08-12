32165dc325f438a112a11d49ba75169ba148b724
mduzairshams <mduzairshams@gmail.com>
2025-08-12 18:36:36 +0530

    Signup component spacing

--- frontend/src/landing_page/signup/Investment.js
+++ frontend/src/landing_page/signup/Investment.js
@@ -2,26 +2,26 @@ import React from "react";
 
 function Investment() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">
+    <div className="container mt-5">
+      <div className="row mt-5">
+        <div className="col-6 text-center">
           <div className="row">
-            <div className="col-3">
+            <div className="col-3 text-center">
               <img src="media/images/stocks-acop.svg" alt="Stocks" />
             </div>
 
-            <div className="col-3">
+            <div className="col-3 text-center">
                 <h3 style={{color:"#3c3c3c"}}>Stocks</h3>
                 <h4 className='mt-3 fs-5 text-muted' style={{color:"#3c3c3c", fontWeight:"normal"}}>Invest in all exchange-listed securities</h4>
             </div>
           </div>
 
           <div className="row">
-            <div className="col-3">
+            <div className="col-3 text-center">
               <img src="media/images/mf-acop.svg" alt="Stocks" />
             </div>
 
-            <div className="col-3">
+            <div className="col-3 text-center">
                 <h3 style={{color:"#3c3c3c"}}>IPO</h3>
                 <h4 className='mt-3 fs-5 text-muted' style={{color:"#3c3c3c", fontWeight:"normal"}}>Apply to the latest IPOs instantly via UPI</h4>
             </div>
@@ -29,26 +29,26 @@ function Investment() {
         </div>
 
 
-        <div className="col-6">
+        <div className="col-6 text-center">
 
 
           <div className="row">
-            <div className="col-3">
+            <div className="col-3 text-center">
               <img src="media/images/ipo-acop.svg" alt="Stocks" />
             </div>
 
-            <div className="col-3">
+            <div className="col-3 text-center">
                 <h3 style={{color:"#3c3c3c"}}>Mutual funds</h3>
                 <h4 className='mt-3 fs-5 text-muted' style={{color:"#3c3c3c", fontWeight:"normal"}}>Invest in commission-free direct mutual funds</h4>
             </div>
           </div>
 
           <div className="row">
-            <div className="col-3">
+            <div className="col-3 text-center">
               <img src="media/images/fo-acop.svg" alt="Stocks" />
             </div>
 
-            <div className="col-3">
+            <div className="col-3 text-center">
                 <h3 style={{color:"#3c3c3c"}}>Futures & options</h3>
                 <h4 className='mt-3 fs-5 text-muted' style={{color:"#3c3c3c", fontWeight:"normal"}}>Hedge and mitigate market risk through simplified F&O trading</h4>
             </div>
@@ -56,7 +56,7 @@ function Investment() {
           </div>
         </div>
       </div>
-      <button className='p-2 fs-4  btn btn-primary mb-5' style={{width:"15%" , margin: "0 auto", fontWeight:"500" }}>Explore Investments </button>
+      <button className=' text-center p-2 fs-4  btn btn-primary mb-5' style={{width:"15%" , margin: "0 auto", fontWeight:"500" }}>Explore Investments </button>
     </div>
   );
 }
--- frontend/src/landing_page/signup/OpenNow.js
+++ frontend/src/landing_page/signup/OpenNow.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 function OpenNow() {
     return ( 
-        <div className="container">
-            <div className="row">
-                <div className="col-6">
+        <div className="container mt-5">
+            <div className="row mt-5 text-center">
+                <div className="col-6 text-center">
                     <img src="media/images/account_open.svg" alt="App_overview" />
 
                 </div>
 
-                <div className="col-6">
+                <div className="col-6 text-center">
                     <h3 style={{color:"#3c3c3c"}}>Signup now</h3>
                     <h5 style={{color:"#878787ff"}}>Or track your existing application</h5>
                     <div className="col-2">
@@ -18,7 +18,7 @@ function OpenNow() {
                         +91
 
                     </div>
-                    <div className="col-4">
+                    <div className="col-4 text-center">
                         <input type="number" placeholder='Enter your mobile number' />
 
                     </div>
